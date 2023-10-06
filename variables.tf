49e77bf034848964127976a0b718a36cc2a6de69
Chinyere12 <chinyereofor@domain.com>
2023-10-06 19:30:21 +0100

    k8 project

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "Default region for my cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "onyxquity-v3-pi"
+}
